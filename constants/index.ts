cac9043e8350ade13b5611d91630e10f3ab9bec9
vikas kumar <vs9917088347@gmail.com>
2025-09-15 23:06:17 +0530

    Initial commit: Add portfolio code

---
+++ constants/index.ts
@@ -0,0 +1,227 @@
+import { FaYoutube, FaFacebook } from "react-icons/fa";
+import {
+  RxDiscordLogo,
+  RxGithubLogo,
+  RxInstagramLogo,
+  RxTwitterLogo,
+  RxLinkedinLogo,
+} from "react-icons/rx";
+
+export const SKILL_DATA = [
+  {
+    skill_name: "HTML",
+    image: "html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "CSS",
+    image: "css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "JavaScript",
+    image: "js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Tailwind CSS",
+    image: "tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React",
+    image: "react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "TypeScript",
+    image: "ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Next.js 14",
+    image: "next.png",
+    width: 80,
+    height: 80,
+  },
+
+
+  {
+    skill_name: "Node.js",
+    image: "node.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "MongoDB",
+    image: "mongodb.png",
+    width: 40,
+    height: 40,
+  },
+] as const;
+
+export const SOCIALS = [
+  {
+    name: "Instagram",
+    icon: RxInstagramLogo,
+    link: "https://www.instagram.com/codebyvikas/",
+  },
+  {
+    name: "Twitter",
+    icon: RxTwitterLogo,
+    link: "https://x.com/VikasKumar76661",
+  },
+] as const;
+
+export const FRONTEND_SKILL = [
+  {
+    skill_name: "HTML",
+    image: "html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "CSS",
+    image: "css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "JavaScript",
+    image: "js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Tailwind CSS",
+    image: "tailwind.png",
+    width: 80,
+    height: 80,
+  },
+
+  {
+    skill_name: "React",
+    image: "react.png",
+    width: 80,
+    height: 80,
+  },
+
+
+  {
+    skill_name: "Next.js 14",
+    image: "next.png",
+    width: 80,
+    height: 80,
+  },
+] as const;
+
+export const BACKEND_SKILL = [
+  {
+    skill_name: "Node.js",
+    image: "node.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Express.js",
+    image: "express.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "MongoDB",
+    image: "mongodb.png",
+    width: 40,
+    height: 40,
+  },
+
+
+] as const;
+
+export const FULLSTACK_SKILL = [
+] as const;
+
+export const OTHER_SKILL = [
+
+] as const;
+
+export const PROJECTS = [
+  {
+    title: "Collaborative Interpreter",
+    
+    image: "/projects/cb-int-img.png",
+    link: "https://v0-collaborative-coding-platform-alpha.vercel.app/",
+  },
+  {
+    title: "Brandscalar",
+
+    image: "/projects/brand.png",
+    link: "https://brandscalar.com/",
+  },
+  {
+    title: "Social-Site",
+   
+    image: "/projects/social.png",
+    link: "https://social-site-by-vikas.vercel.app/",
+  },
+] as const;
+
+export const FOOTER_DATA = [
+  {
+    title: "Community",
+    data: [
+
+      {
+        name: "GitHub",
+        icon: RxGithubLogo,
+        link: "https://github.com/Vikaskumar242004",
+      },
+
+    ],
+  },
+  {
+    title: "Social Media",
+    data: [
+
+      {
+        name: "Linkedin",
+        icon: RxLinkedinLogo,
+        link: "https://www.linkedin.com/in/vikas-kumar-3273022ab/",
+      },
+    ],
+  },
+  {
+    title: "About",
+    data: [
+   
+      {
+        name: "Contact Me",
+        icon: null,
+        link: "mailto:vs9917088347@gmail.com",
+      },
+    ],
+  },
+] as const;
+
+export const NAV_LINKS = [
+  {
+    title: "About me",
+    link: "#about-me",
+  },
+  {
+    title: "Skills",
+    link: "#skills",
+  },
+  {
+    title: "Projects",
+    link: "#projects",
+  },
+] as const;
+
+
